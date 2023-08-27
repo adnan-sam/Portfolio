4fec55af135d00cf4e79132534eef224b76d2bec
Adnan Sameer <adnansameer400@gmail.com>
2023-08-27 22:38:47 +0530

    Fixed 80% of the day/night

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {FaGithub} from 'react-icons/fa';
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/adnan-sameer-199ba1223/' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com/adnan-sam' target='_blank'><FaGithub/></a>
-        <a href='https://leetcode.com/adnan_sam_/' target='_blank'><BsCodeSlash/></a>
+        <a href='https://www.linkedin.com/in/adnan-sameer-199ba1223/' target='_blank'><BsLinkedin className='so_logo'/></a>
+        <a href='https://github.com/adnan-sam' target='_blank'><FaGithub className='so_logo'/></a>
+        <a href='https://leetcode.com/adnan_sam_/' target='_blank'><BsCodeSlash className='so_logo'/></a>
     </div>
   )
 }
