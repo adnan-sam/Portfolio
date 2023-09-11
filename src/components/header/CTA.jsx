430d44e3327c7ccae54aa0519662c4ee3938350e
Adnan Sameer <adnansameer400@gmail.com>
2023-09-12 00:57:03 +0530

    Resume-download-msg fix

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import resume from '../../assets/AdnanSameer_Resume.pdf';
+import { AiOutlineDownload } from 'react-icons/ai';
 
 const CTA = () => {
+
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  const handleDownloadClick = () => {
+    // Simulate the download process with a delay (you can replace this with actual download logic)
+    setIsDownloading(true);
+    setTimeout(() => {
+      setIsDownloading(false);
+    }, 3000); // 3 seconds delay (adjust as needed)
+  };
+
   return (
     <div className='cta'>
-        <a href={resume} download className='btn'>Resume</a>
+        <a href={resume} download onClick={handleDownloadClick} className={`btn ${isDownloading ? 'downloading' : ''}`}>
+        {isDownloading ? 'Downloading...' : 'Resume'}
+        </a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
